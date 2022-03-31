f0a11c398ec86cc09895f562716b4b4b2f992c94
Robert Williams <rcw0086@gmail.com>
2022-03-30 20:04:22 -0500

    finished exercise e, references and borrowing

--- exercise/e_ownership_references/src/main.rs
+++ exercise/e_ownership_references/src/main.rs
@@ -9,35 +9,60 @@ fn main() {
         std::process::exit(-1);
     });
 
+    
+    println!("My original arg is {}", arg);
+    inspect(&arg); // -> note, Rust creates a reference here, with my call
+    change(&mut arg); // -> note, Rust creates a mutable reference here, with my call
+    println!("My mutated arg is {}", arg); // MUTATION has happened here; prints change() output
+
     // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
     // prints whether the contents of the String is plural or singular. Then uncomment and run this
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
-    //
-    //inspect(&arg);
+    fn inspect(arg: &String) {
+        if arg.ends_with("s") {
+            println!("plural!");
+        } else {
+            println!("singular");
+        };
+    }
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    fn change(arg: &mut String) {
+        if !arg.ends_with("s") { arg.push_str("s"); }
+        println!("I have many {}", arg);
+    }
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
-    //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+    
+    fn eat(s: String) -> bool {
+        return if s.starts_with("b") && s.ends_with("s") {
+            true
+        } else {
+            false
+        };
+    }
+
+    if eat(arg) {
+       println!("Might be bananas");
+    } else {
+       println!("Not bananas");
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
     // Challenge: Write a function "add" that takes *references* to two integer arguments,
     // dereferences them and adds them together, and returns the result.
     //
-    // println!("1 + 2 = {}, even via references", add(&1, &2));
+    fn add (x: &i32, y: &i32) -> i32 {
+        x + y
+    }
+
+    println!("1 + 2 = {}, even via references", add(&1, &2));
 }
